ae7dc4f3b99693dfcb033ced547c1dd67e818841
Jean-Yves Bateman <jeanyves.bateman@gmail.com>
2017-07-18 16:10:21 +0200

    added write message function

--- plug.go
+++ plug.go
@@ -18,6 +18,20 @@ type Plug struct {
 	notif *gatt.Characteristic
 }
 
+func (pl *Plug) SendMessage(b []byte) {
+	for i := 0; i < len(b); i += 20 {
+		end := i + 20
+		if end > len(b) {
+			end = len(b)
+		}
+		err := pl.per.WriteCharacteristic(pl.cmd, b[i:end], true)
+		if err != nil {
+			fmt.Println("ERROR:", err)
+		}
+	}
+}
+	
 func (pl *Plug) On() {
 	b := CreateMessage([]byte{0x03, 0x00, 0x01, 0x00, 0x00})
+	pl.SendMessage(b)
 }
